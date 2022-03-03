f38a8af107c4984e1266e3bc97ea3774e5a817aa
simen <simen@host.docker.internal>
2022-03-03 20:29:52 +0100

    Create ferdig

--- src/no/hvl/dat110/messages/CreateTopicMsg.java
+++ src/no/hvl/dat110/messages/CreateTopicMsg.java
@@ -4,9 +4,28 @@ public class CreateTopicMsg extends Message {
 	
 	// message sent from client to create topic on the broker
 	
+	String topicName;
+	
+
+	
 	// TODO: 
 	// Implement object variables - a topic is required
+	public CreateTopicMsg(String user, String topicName) {
+		super(MessageType.CREATETOPIC, user);
+		this.topicName=topicName;
+	}
 	
+	public String getTopic(){
+		return topicName;
+	}
+	
+	public void setTopic(String topicName) {
+		this.topicName=topicName;
+	}
+	
+	public String toString() {
+		return "Topic: " + topicName + ", Type: " + getType() + ", User: " + getUser();
+	}
 	// Constructor, get/set-methods, and toString method
     // as described in the project text	
 }
