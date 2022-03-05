d7b55bf06ed0275b05132ffe7b028c0e5fe7b0d9
simen <simen@host.docker.internal>
2022-03-05 15:53:43 +0100

    Fikset for-løkke i Display

--- src/no/hvl/dat110/iotsystem/DisplayDevice.java
+++ src/no/hvl/dat110/iotsystem/DisplayDevice.java
@@ -6,15 +6,15 @@ import no.hvl.dat110.messages.PublishMsg;
 import no.hvl.dat110.common.TODO;
 
 public class DisplayDevice {
-	
+
 	private static final int COUNT = 10;
-		
-	public static void main (String[] args) {
-		
+
+	public static void main(String[] args) throws InterruptedException {
+
 		System.out.println("Display starting ...");
-		
+
 		// TODO - START
-				
+
 		// create a client object and use it to
 		Client display = new Client("Display", "localhost", 8080);
 		// - connect to the broker - use "display" as the username
@@ -24,17 +24,24 @@ public class DisplayDevice {
 		// - subscribe to the topic
 		display.subscribe(Common.TEMPTOPIC);
 		// - receive messages on the topic
-		display.receive();
+		for (int i = 0; i < COUNT; i++) {
+
+			PublishMsg dispMsg = (PublishMsg) display.receive();
+			
+			System.out.println(dispMsg + " degrees");
+			
+			Thread.sleep(TemperatureDevice.updateFreq);
+		}
 		// - unsubscribe from the topic
 		display.unsubscribe(Common.TEMPTOPIC);
 		// - disconnect from the broker
 		display.disconnect();
-		
+
 		// TODO - END
-		
+
 		System.out.println("Display stopping ... ");
-		
+
 		throw new UnsupportedOperationException(TODO.method());
-		
+
 	}
 }
